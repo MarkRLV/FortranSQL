a80b126b434160519236604fe1322ef1753b3e9f
Mark Robinson <markrlv@aol.com>
2021-03-30 14:21:53 -0600

    Add project files.

---
+++ FortranSQL.fs
@@ -0,0 +1,27 @@
+﻿open System
+open MySql.Data.MySqlClient
+
+[<EntryPoint>]
+let main argv =
+    use myConnection = new MySqlConnection()
+    use myCommand = new MySqlCommand()
+    
+    let ConnectionString = "server=(someIPAddress); port=3306; uid=Me; password=MyPassword;"
+    let myConnection : MySqlConnection = new MySqlConnection(ConnectionString)
+    let myCommand : MySqlCommand = new MySqlCommand()
+    myConnection.Open()
+    
+    myCommand.Connection <- myConnection
+    myCommand.CommandText <- "SELECT * FROM gcb.devicecodes"
+
+    let reader = myCommand.ExecuteReader()
+
+    while reader.Read() do
+        let Code = reader.GetString(reader.GetOrdinal("Code"))
+        let GCBCode = reader.GetString(reader.GetOrdinal("GCBCode"))
+        let DeviceName = reader.GetString(reader.GetOrdinal("DeviceName"))
+        Console.WriteLine("{0} {1} {2}", Code, GCBCode, DeviceName)
+
+    myConnection.Close()
+
+    0 // return an integer exit code
